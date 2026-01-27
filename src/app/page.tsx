722fff017416289ef82e78b34e3a993abbde1d8f
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2026-01-27 14:39:59 -0300

    set css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 import { ProjectService } from '@/services/projects/project.service';
-import { TechnologyService } from '@/services/technology/technology.service';
 import { Table } from '@/components/Table';
 import { TableCell, TableRow } from '@/components/ui/table';
 import { Badge } from '@/components/ui/badge'; // Supondo que você tenha um componente Badge
 
 export default async function Home() {
 
-  const [projects, technologies] = await Promise.all([
-    ProjectService.findAll(),
-    TechnologyService.findAll(),
-  ])
-  console.log(JSON.stringify(projects, null, 2));
+  const projects = await ProjectService.findAll()    
 
   return (
     <main className="">
@@ -35,17 +30,6 @@ export default async function Home() {
           </TableRow>
         ))}
       </Table>
-      {/* <Table
-        headers={['Nome', 'Release']}
-        caption='Techs'
-      >
-        {technologies.map(tech => (
-          <TableRow key={tech.id}>
-            <TableCell>{tech.name}</TableCell>
-            <TableCell>{tech.release}</TableCell>
-          </TableRow>
-        ))}
-      </Table> */}
 
     </main>
   );
